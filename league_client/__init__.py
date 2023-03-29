d46759ddd34758d64fd5543252a04c4eb2bfa299
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2023-03-29 13:38:39 +0545

    Cleans __init__.py

--- league_client/__init__.py
+++ league_client/__init__.py
@@ -1,18 +1,1 @@
-from . import chat
-from . import disenchant
-from . import friend_requests
-from . import friends
-from . import gifting
-from . import inventory
-from . import logout
-from . import loot
-from . import match_history
-from . import ranked
-from . import riot
-from . import shortcuts
-from . import summoner
-from . import token
-from . import username
-from . import wallet
-
-__version__ = '1.0.7'
+__version__ = '1.0.8'
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='league-client',
-    version='1.0.7',
+    version='1.0.8',
     author='Pradish Bijukchhe',
     author_email='pradishbijukchhe@gmail.com',
     description='Python package to communicate with riot client and league client',
