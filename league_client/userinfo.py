78349e9419aa4839b173675debb46ed58a9afb4f
Chirag Lamsal <chirag.lamsal@gmail.com>
2023-05-22 11:29:00 +0545

    fixes formatting

--- league_client/userinfo.py
+++ league_client/userinfo.py
@@ -8,7 +8,7 @@ from .rso import get_basic_auth
 
 async def parse_auth_code(session, data, proxy, proxy_auth):
     async with session.post(
-        "https://auth.riotgames.com/api/v1/authorization",
+        'https://auth.riotgames.com/api/v1/authorization',
         proxy=proxy,
         proxy_auth=proxy_auth,
         json=data,
@@ -22,7 +22,7 @@ async def parse_auth_code(session, data, proxy, proxy_auth):
 
 async def parse_userinfo(session, headers, proxy, proxy_auth):
     async with session.post(
-        "https://auth.riotgames.com/userinfo",
+        'https://auth.riotgames.com/userinfo',
         proxy=proxy,
         proxy_auth=proxy_auth,
         json={},
@@ -30,23 +30,23 @@ async def parse_userinfo(session, headers, proxy, proxy_auth):
     ) as res:
         if not res.ok:
             logger.debug(res.status)
-            return {"error": "Failed to parse userinfo"}, 1
+            return {'error': 'Failed to parse userinfo'}, 1
         return await res.json(), None
 
 
 def _extract_tokens(data: str) -> str:
-    """Extract tokens from data"""
+    '''Extract tokens from data'''
 
     pattern = re.compile(
-        "access_token=((?:[a-zA-Z]|\d|\.|-|_)*).*id_token=((?:[a-zA-Z]|\d|\.|-|_)*).*expires_in=(\d*)"
+        'access_token=((?:[a-zA-Z]|\d|\.|-|_)*).*id_token=((?:[a-zA-Z]|\d|\.|-|_)*).*expires_in=(\d*)'
     )
-    response = pattern.findall(data["response"]["parameters"]["uri"])[0]
+    response = pattern.findall(data['response']['parameters']['uri'])[0]
     return response
 
 
 async def parse_access_token(session, data, proxy, proxy_auth):
     async with session.put(
-        "https://auth.riotgames.com/api/v1/authorization",
+        'https://auth.riotgames.com/api/v1/authorization',
         proxy=proxy,
         proxy_auth=proxy_auth,
         json=data,
@@ -54,30 +54,30 @@ async def parse_access_token(session, data, proxy, proxy_auth):
     ) as res:
         if not res.ok:
             logger.debug(res.status)
-            return {"error": "Failed to get access token"}, 1
+            return {'error': 'Failed to get access token'}, 1
         data = await res.json()
-        response_type = data["type"]
-        if response_type == "response":
-            logger.info("Parsing userinfo...")
+        response_type = data['type']
+        if response_type == 'response':
+            logger.info('Parsing userinfo...')
             response = _extract_tokens(data)
             access_token = response[0]
             headers = {
-                "Content-Type": "application/json",
-                "Authorization": f"Bearer {access_token}",
+                'Content-Type': 'application/json',
+                'Authorization': f'Bearer {access_token}',
             }
             data, error = await parse_userinfo(session, headers, proxy, proxy_auth)
             if error:
-                logger.info("Failed.")
+                logger.info('Failed.')
                 return data, 1
-            logger.info("Success.")
+            logger.info('Success.')
             return data, None
-        elif response_type == "multifactor":
-            return {"error": "Multifactor authentication"}, 1
-        elif response_type == "auth" and data["error"] == "auth_failure":
-            return {"error": "Wrong password"}, 1
-        elif response_type == "auth" and data["error"] == "rate_limited":
-            return {"error": "Rate limited"}, 1
-        return {"error": "Got response type: {response_type}"}, 1
+        elif response_type == 'multifactor':
+            return {'error': 'Multifactor authentication'}, 1
+        elif response_type == 'auth' and data['error'] == 'auth_failure':
+            return {'error': 'Wrong password'}, 1
+        elif response_type == 'auth' and data['error'] == 'rate_limited':
+            return {'error': 'Rate limited'}, 1
+        return {'error': 'Got response type: {response_type}'}, 1
 
 async def get_userinfo(
     username, password, proxy=None, proxy_user=None, proxy_pass=None
@@ -85,32 +85,32 @@ async def get_userinfo(
     try:
         proxy_auth = get_basic_auth(proxy_user, proxy_pass)
         async with ClientSession() as session:
-            logger.info("Parsing authorization code...")
+            logger.info('Parsing authorization code...')
             data = {
-                "client_id": "riot-client",
-                "nonce": "1",
-                "redirect_uri": "http://localhost/redirect",
-                "scope": "openid ban",
-                "response_type": "token id_token",
+                'client_id': 'riot-client',
+                'nonce': '1',
+                'redirect_uri': 'http://localhost/redirect',
+                'scope': 'openid ban',
+                'response_type': 'token id_token',
             }
             if not await parse_auth_code(session, data, proxy, proxy_auth):
-                logger.info("Failed.")
-                return {"error": "Failed to parse authorization code"}, 1
-            logger.info("Success.")
+                logger.info('Failed.')
+                return {'error': 'Failed to parse authorization code'}, 1
+            logger.info('Success.')
 
-            logger.info("Getting access token...")
+            logger.info('Getting access token...')
             data = {
-                "type": "auth",
-                "username": username,
-                "password": password,
-                "remember": True,
+                'type': 'auth',
+                'username': username,
+                'password': password,
+                'remember': True,
             }
             data, error = await parse_access_token(session, data, proxy, proxy_auth)
             if error:
-                logger.info("Failed.")
+                logger.info('Failed.')
                 return data, 1
-            logger.info("Success.")
+            logger.info('Success.')
             return data, None
     except Exception as exp:
         logger.error(f'Got exception type: {exp.__class__.__name__}')
-        return {"error": "Exception occurred"}, 1
+        return {'error': 'Exception occurred'}, 1
