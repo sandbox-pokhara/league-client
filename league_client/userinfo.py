84a601b6ced9188fa6b0e2fc8a138e2b5edff6b0
Chirag Lamsal <chirag.lamsal@gmail.com>
2023-05-23 13:14:14 +0545

    moves to utils

--- league_client/userinfo.py
+++ league_client/userinfo.py
@@ -1,70 +1,8 @@
-import re
-
 from .logger import logger
-from .rso import HEADERS
 from .rso import ClientSession
 from .rso import get_basic_auth
 from .rso_auth import parsing_auth_code
-
-
-async def parse_userinfo(session, headers, proxy, proxy_auth):
-    async with session.post(
-        'https://auth.riotgames.com/userinfo',
-        proxy=proxy,
-        proxy_auth=proxy_auth,
-        json={},
-        headers=headers,
-    ) as res:
-        if not res.ok:
-            logger.debug(res.status)
-            return {'error': 'Failed to parse userinfo'}, 1
-        return await res.json(), None
-
-
-def _extract_tokens(data: str) -> str:
-    '''Extract tokens from data'''
-
-    pattern = re.compile(
-        'access_token=((?:[a-zA-Z]|\d|\.|-|_)*).*id_token=((?:[a-zA-Z]|\d|\.|-|_)*).*expires_in=(\d*)'
-    )
-    response = pattern.findall(data['response']['parameters']['uri'])[0]
-    return response
-
-
-async def parse_access_token(session, data, proxy, proxy_auth):
-    async with session.put(
-        'https://auth.riotgames.com/api/v1/authorization',
-        proxy=proxy,
-        proxy_auth=proxy_auth,
-        json=data,
-        headers=HEADERS,
-    ) as res:
-        if not res.ok:
-            logger.debug(res.status)
-            return {'error': 'Failed to get access token'}, 1
-        data = await res.json()
-        response_type = data['type']
-        if response_type == 'response':
-            logger.info('Parsing userinfo...')
-            response = _extract_tokens(data)
-            access_token = response[0]
-            headers = {
-                'Content-Type': 'application/json',
-                'Authorization': f'Bearer {access_token}',
-            }
-            data, error = await parse_userinfo(session, headers, proxy, proxy_auth)
-            if error:
-                logger.info('Failed.')
-                return data, 1
-            logger.info('Success.')
-            return data, None
-        elif response_type == 'multifactor':
-            return {'error': 'Multifactor authentication'}, 1
-        elif response_type == 'auth' and data['error'] == 'auth_failure':
-            return {'error': 'Wrong password'}, 1
-        elif response_type == 'auth' and data['error'] == 'rate_limited':
-            return {'error': 'Rate limited'}, 1
-        return {'error': 'Got response type: {response_type}'}, 1
+from .utils import parse_access_token
 
 async def get_userinfo(
     username, password, proxy=None, proxy_user=None, proxy_pass=None
