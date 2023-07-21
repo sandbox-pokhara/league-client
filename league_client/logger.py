e9b18a98e5658d8d363be4ee9d6dc9d0d4d8d1ca
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2023-07-21 13:51:56 +0545

    refactor: update logger module

--- league_client/logger.py
+++ league_client/logger.py
@@ -4,26 +4,39 @@ import sys
 from logging import StreamHandler
 from logging.handlers import RotatingFileHandler
 
-LOG_FILE_PATH = "logs/main.log"
-os.makedirs("logs", exist_ok=True)
+# register in pylance auto import
+__all__ = ["logger"]
 
-logger = logging.Logger("league-client")
-formatter = logging.Formatter(
-    "%(asctime)s - %(levelname)s - %(message)s",
-    "%m/%d/%Y %I:%M:%S %p",
-)
 
-stream_handler = StreamHandler(sys.stdout)
-file_handler = RotatingFileHandler(
-    LOG_FILE_PATH, maxBytes=1_048_576, backupCount=100
-)  # 100 MB
+def create_logger(file_path, name, formatter=None, stream=True, rotating_file=True):
+    dirname = os.path.dirname(file_path)
+    os.makedirs(dirname, exist_ok=True)
 
-file_handler.setFormatter(formatter)
-stream_handler.setFormatter(formatter)
+    logger = logging.getLogger(name)
+    if formatter is None:
+        formatter = logging.Formatter(
+            "%(asctime)s - %(levelname)s - %(message)s",
+            "%m/%d/%Y %I:%M:%S %p",
+        )
+    logger.setLevel(logging.DEBUG)
 
-file_handler.setLevel(logging.DEBUG)
-stream_handler.setLevel(logging.INFO)
-logger.setLevel(logging.DEBUG)
+    if stream:
+        stream_handler = StreamHandler(sys.stdout)
+        stream_handler.setFormatter(formatter)
+        stream_handler.setLevel(logging.INFO)
+        logger.addHandler(stream_handler)
 
-logger.addHandler(file_handler)
-logger.addHandler(stream_handler)
+    if rotating_file:
+        file_handler = RotatingFileHandler(
+            file_path, maxBytes=1_048_576, backupCount=100
+        )  # 100 x 1 MB
+
+        file_handler.setFormatter(formatter)
+        file_handler.setLevel(logging.DEBUG)
+        logger.addHandler(file_handler)
+
+    return logger
+
+
+file_path = os.path.join("logs", "league-client.log")
+logger = create_logger(file_path, "league-client")
