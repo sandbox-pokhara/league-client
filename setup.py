4417c28d3bb329799a4248fc23ae1256fc05b022
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2023-07-22 21:04:44 +0545

    chore: release 1.0.28

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="league-client",
-    version="1.0.27",
+    version="1.0.28",
     author="Pradish Bijukchhe",
     author_email="pradishbijukchhe@gmail.com",
     description="Python package to communicate with riot client and league client",
