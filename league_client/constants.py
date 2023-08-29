e603df488bc39b1404ad88508e36d08368d8007f
anchal ghale <anchalghale@gmail.com>
2023-08-29 17:51:11 +0545

    fix: fix user agent

--- league_client/constants.py
+++ league_client/constants.py
@@ -1,5 +1,5 @@
 SITE_URL = "https://authenticate.riotgames.com/api/v1/login"
 USER_AGENT = (
-    "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36"
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36"
     " (KHTML, like Gecko) RiotClient/70.0.0 (CEF 74) Safari/537.36"
 )
