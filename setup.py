6614516a59f5eca88b87305fe91a4ce19e4108d2
Anchal Gurung <anchalghale@gmail.com>
2023-09-25 17:20:08 +0545

    chore: release 1.0.56

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="league-client",
-    version="1.0.55",
+    version="1.0.56",
     author="Pradish Bijukchhe",
     author_email="pradishbijukchhe@gmail.com",
     description="Python package to communicate with riot client and league client",
@@ -29,6 +29,6 @@ setuptools.setup(
         "league-connection",
         "psutil",
         "aiohttp",
-        "ucaptcha>=1.0.6",
+        "ucaptcha>=1.0.7",
     ],
 )
