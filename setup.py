d0c1239ab2e1c1f9470a91585fe818926d7a2b2c
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2023-09-27 16:38:53 +0545

    chore: release 1.0.57

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="league-client",
-    version="1.0.56",
+    version="1.0.57",
     author="Pradish Bijukchhe",
     author_email="pradishbijukchhe@gmail.com",
     description=(
