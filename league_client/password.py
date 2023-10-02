515ddd2849fc45abb6737c3af7df07f5a2922d66
Chirag Lamsal <chirag.lamsal@gmail.com>
2023-10-02 14:57:40 +0545

    fix: function naming

--- league_client/password.py
+++ league_client/password.py
@@ -1,9 +1,14 @@
 import re
 from copy import copy
 
+from league_client.exceptions import RSOAuthorizeError
+
 from .logger import logger
-from .rso import HEADERS, ClientSession, get_basic_auth
-from .rso_auth import parsing_auth_code, rso_authorize
+from .rso import HEADERS
+from .rso import ClientSession
+from .rso import get_basic_auth
+from .rso_auth import parsing_auth_code
+from .rso_auth import rso_authorize
 
 accountodactyl = {
     "scope": (
@@ -84,6 +89,80 @@ async def post_change_password(
         return res.ok
 
 
+async def post_send_verify_link(
+    session,
+    csrf_token,
+    email,
+    proxy,
+    proxy_user=None,
+    proxy_pass=None,
+):
+    data = {
+        "email": email,
+    }
+    headers = copy(HEADERS)
+    headers["csrf-token"] = csrf_token
+    headers["referer"] = "https://account.riotgames.com/"
+    async with session.post(
+        "https://account.riotgames.com/api/account/v1/user/email/",
+        proxy=proxy,
+        proxy_auth=get_basic_auth(proxy_user, proxy_pass),
+        json=data,
+        headers=headers,
+    ) as res:
+        if not res.ok:
+            logger.debug(res.status)
+            logger.debug(await res.text())
+            return False
+        return True
+
+
+async def post_change_password_and_send_verify_link(
+    session,
+    csrf_token,
+    current_password,
+    new_password,
+    email,
+    proxy,
+    proxy_user=None,
+    proxy_pass=None,
+):
+    data1 = {
+        "currentPassword": current_password,
+        "password": new_password,
+    }
+    data2 = {
+        "email": email,
+    }
+    headers = copy(HEADERS)
+    headers["csrf-token"] = csrf_token
+    # If referer is not set, it responseds with 403, not logged in error
+    headers["referer"] = "https://account.riotgames.com/"
+    async with session.put(
+        "https://account.riotgames.com/api/account/v1/user/password",
+        proxy=proxy,
+        proxy_auth=get_basic_auth(proxy_user, proxy_pass),
+        json=data1,
+        headers=headers,
+    ) as res:
+        if not res.ok:
+            logger.debug(res.status)
+            logger.debug(await res.text())
+            return False, False
+    async with session.post(
+        "https://account.riotgames.com/api/account/v1/user/email/",
+        proxy=proxy,
+        proxy_auth=get_basic_auth(proxy_user, proxy_pass),
+        json=data2,
+        headers=headers,
+    ) as res:
+        if not res.ok:
+            logger.debug(res.status)
+            logger.debug(await res.text())
+            return True, False
+        return True, True
+
+
 async def change_password(
     username,
     password,
@@ -130,3 +209,148 @@ async def change_password(
         ):
             return False
         return True
+
+
+async def check_password(
+    username,
+    password,
+    proxy=None,
+    proxy_user=None,
+    proxy_pass=None,
+):
+    """Check whether the password is correct
+
+    Args:
+        username (str): account username
+        password (str): account password
+        email (str): valid email address to send the verify link to
+        proxy (str, optional): proxy url. Defaults to None.
+        proxy_user (str, optional): proxy username. Defaults to None.
+        proxy_pass (str, optional): proxy password. Defaults to None.
+
+    Returns:
+        True: password is correct
+        False: password is incorrect
+    """
+    async with ClientSession() as session:
+        proxy_auth = get_basic_auth(proxy_user, proxy_pass)
+        if not await parsing_auth_code(
+            session, accountodactyl, proxy, proxy_user, proxy_pass
+        ):
+            return False
+        try:
+            await rso_authorize(session, username, password, proxy, proxy_auth)
+        except RSOAuthorizeError as e:
+            if e.code == "WRONG_PASSWORD":
+                return False
+            raise
+        return True
+
+
+async def send_verify_link(
+    username, password, email, proxy=None, proxy_user=None, proxy_pass=None
+):
+    """Send verify link to email (doesn't verify the email)
+
+    Args:
+        username (str): account username
+        password (str): account password
+        email (str): valid email address to send the verify link to
+        proxy (str, optional): proxy url. Defaults to None.
+        proxy_user (str, optional): proxy username. Defaults to None.
+        proxy_pass (str, optional): proxy password. Defaults to None.
+
+    Returns:
+        False: Failed to send the verify link
+        True: Successfully sent the verify link
+    """
+    async with ClientSession() as session:
+        proxy_auth = get_basic_auth(proxy_user, proxy_pass)
+        if not await parsing_auth_code(
+            session, accountodactyl, proxy, proxy_user, proxy_pass
+        ):
+            return False
+        data = await rso_authorize(
+            session, username, password, proxy, proxy_auth
+        )
+        url = data["response"]["parameters"]["uri"]
+        if url is None:
+            return False
+
+        if not await redirect(session, url, proxy, proxy_user, proxy_pass):
+            return False
+
+        csrf_token = await get_csrf_token(
+            session, proxy, proxy_user, proxy_pass
+        )
+        if csrf_token is None:
+            return False
+
+        if not await post_send_verify_link(
+            session,
+            csrf_token,
+            email,
+            proxy,
+            proxy_user,
+            proxy_pass,
+        ):
+            return False
+        return True
+
+
+async def change_password_and_send_verify_link(
+    username,
+    password,
+    new_password,
+    email,
+    proxy=None,
+    proxy_user=None,
+    proxy_pass=None,
+):
+    """Change password and send verify link to email
+
+    Args:
+        username (str): account username
+        password (str): account password
+        new_password (str): new password to change to
+        email (str): valid email address to send the verify link to
+        proxy (str, optional): proxy url. Defaults to None.
+        proxy_user (str, optional): proxy username. Defaults to None.
+        proxy_pass (str, optional): proxy password. Defaults to None.
+
+    Returns:
+        (bool, bool): (password_changed, verify_link_sent)
+        # NOTE (True, False): password is changed but verify link could not be sent
+    """
+    async with ClientSession() as session:
+        proxy_auth = get_basic_auth(proxy_user, proxy_pass)
+        if not await parsing_auth_code(
+            session, accountodactyl, proxy, proxy_user, proxy_pass
+        ):
+            return False, False
+        data = await rso_authorize(
+            session, username, password, proxy, proxy_auth
+        )
+        url = data["response"]["parameters"]["uri"]
+        if url is None:
+            return False, False
+
+        if not await redirect(session, url, proxy, proxy_user, proxy_pass):
+            return False, False
+
+        csrf_token = await get_csrf_token(
+            session, proxy, proxy_user, proxy_pass
+        )
+        if csrf_token is None:
+            return False, False
+
+        return await post_change_password_and_send_verify_link(
+            session,
+            csrf_token,
+            password,
+            new_password,
+            email,
+            proxy,
+            proxy_user,
+            proxy_pass,
+        )
