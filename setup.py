f0dd73d06a3e7344063f60738b7878c4ed47e06e
Anchal Gurung <anchalghale@gmail.com>
2023-10-11 10:40:05 +0545

    chore: release 1.0.59

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="league-client",
-    version="1.0.58",
+    version="1.0.59",
     author="Pradish Bijukchhe",
     author_email="pradishbijukchhe@gmail.com",
     description=(
@@ -33,6 +33,6 @@ setuptools.setup(
         "league-connection",
         "psutil",
         "aiohttp",
-        "ucaptcha>=1.0.7",
+        "ucaptcha>=1.0.8",
     ],
 )
