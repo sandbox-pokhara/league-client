4f1a89c42e4ee058a425b9ac3cac8f77cdaa7feb
Chirag Lamsal <chirag.lamsal@gmail.com>
2024-02-07 10:36:07 +0545

    chore: release 1.0.63 (#23)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="league-client",
-    version="1.0.62",
+    version="1.0.63",
     author="Pradish Bijukchhe",
     author_email="pradishbijukchhe@gmail.com",
     description=(
