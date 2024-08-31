ea67271aae9b1629982ecc7109a0cafd4f8659b6
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2024-08-31 16:43:35 +0545

    chore: bump version to 1.0.70

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="league-client",
-    version="1.0.69",
+    version="1.0.70",
     author="Pradish Bijukchhe",
     author_email="pradishbijukchhe@gmail.com",
     description=(
